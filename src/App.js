c73ecce1db9e4cbe2170d8dbf77555c9066cc085
Giovanni Flores <geflores5@gmail.com>
2019-02-18 22:36:50 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './config/configureStore';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const store = configureStore();
 
-export default App;
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
+
